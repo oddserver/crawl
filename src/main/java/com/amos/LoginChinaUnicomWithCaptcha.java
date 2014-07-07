eba0a69e0787721e217e63c977b5050870ef5217
liyayun <amosli@infomorrow.com>
2014-07-07 17:18:54 +0800

    fix bugs

--- src/main/java/com/amos/LoginChinaUnicomWithCaptcha.java
+++ src/main/java/com/amos/LoginChinaUnicomWithCaptcha.java
@@ -12,6 +12,7 @@ import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.*;
 import org.apache.http.util.EntityUtils;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,12 +24,12 @@ public class LoginChinaUnicomWithCaptcha {
 
     public static void main(String args[]) throws Exception {
 
+        String name =  "13167081006";
+        String pwd = "007745";
+
         //生成验证码的链接
         String createCaptchaUrl = "http://uac.10010.com/portal/Service/CreateImage";
         HttpClient httpClient = new DefaultHttpClient();
-
-        String name =  "中国联通手机号码";
-        String pwd = "手机服务密码";
 
         //这里可自定义所需要的cookie
         CookieStore cookieStore = new BasicCookieStore();
@@ -43,7 +44,7 @@ public class LoginChinaUnicomWithCaptcha {
 
         if (capthcaResponse.getStatusLine().getStatusCode() == 200) {
             //将验证码写入本地
-            Tools.saveToLocal(capthcaResponse.getEntity(), "chinaunicom.capthca." + System.currentTimeMillis());
+            Tools.saveToLocal(capthcaResponse.getEntity(), "chinaunicom.capthca." + System.currentTimeMillis()+".png");
         }
 
 
@@ -55,15 +56,17 @@ public class LoginChinaUnicomWithCaptcha {
         do {
             //输入验证码,读入键盘输入
             //1)
-            InputStream inputStream = System.in;
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            System.out.println("请输入验证码:");
-            capthca = bufferedReader.readLine();
+//            InputStream inputStream = System.in;
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//            System.out.println("请输入验证码:");
+//            capthca = bufferedReader.readLine();
+
+            capthca=JOptionPane.showInputDialog("请输入图片验证码:");
 
             //2)
             //Scanner scanner = new Scanner(System.in);
             //capthca = scanner.next();
-
+           // http://uac.10010.com/portal/Service/CtaIdyChk?callback=jsonp1404716227598&verifyCode=4m3e&verifyType=1
             String verifyCaptchaUrl = "http://uac.10010.com/portal/Service/CtaIdyChk?verifyCode=" + capthca + "&verifyType=1";
             HttpGet verifyCapthcaGet = new HttpGet(verifyCaptchaUrl);
             verifyResponse = httpClient.execute(verifyCapthcaGet);
@@ -75,6 +78,9 @@ public class LoginChinaUnicomWithCaptcha {
                 }
             }
         } while (!EntityUtils.toString(verifyResponse.getEntity()).contains("true"));
+
+//        https://uac.10010.com/portal/Service/MallLogin?callback=jsonp1404716227600&userName=13167081002&password=001122&pwdType=01&productType=01&verifyCode=xhx3&redirectType=03&areaCode=&arrcity=%E5%9C%B0%E5%8C%BA&uvc=3zm50fcae32b5fa8cb5795d5b335cd85d2cv4c
+
 
         //登录
         String loginurl = "https://uac.10010.com/portal/Service/MallLogin?userName=" + name + "&password=" + pwd + "&pwdType=01&productType=01&verifyCode=" + capthca + "&redirectType=03&uvc=" + uvc;
